40cd71e62d93d4d9e9fa33e6869f10c80ff4b049
ChrisMickelson <chrimson76@hotmail.com>
2018-01-31 06:34:13 -0600

    Update RadiusPositionable.java

--- core/src/net/sf/openrocket/rocketcomponent/position/RadiusPositionable.java
+++ core/src/net/sf/openrocket/rocketcomponent/position/RadiusPositionable.java
@@ -1,9 +1,9 @@
 package net.sf.openrocket.rocketcomponent.position;
 
 public interface RadiusPositionable {
-	
-	public double getRadialOffset();
 	public boolean getAutoRadialOffset();	
+	public double getRadialOffset();
+	public void setAutoRadialOffset( final boolean auto );
 	public void setRadialOffset(final double radius);
-	
 }
+
